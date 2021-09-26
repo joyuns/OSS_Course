85a3863e3b20c4dac5d04339b5a9ece242da870b
joyuns <joaenlim@naver.com>
2021-09-27 02:56:06 +0900

    Create covid19_statistics.py

---
+++ covid19.statistics.py/covid19_statistics.py
@@ -0,0 +1,43 @@
+### Korean Population by Region
+* Total population: 51669716
+| Region | Population | Ratio (%) |
+| ------ | ---------- | --------- |
+| Seoul | 9550227 | 18.5 |
+| Gyeongi | 13530519 | 26.2 |
+| Busan | 3359527 | 6.5 |
+| Gyeongnam | 3322373 | 6.4 |
+| Incheon | 2938429 | 5.7 |
+| Gyeongbuk | 2630254 | 5.1 |
+| Daegu | 2393626 | 4.6 |
+| Chungnam | 2118183 | 4.1 |
+| Jeonnam | 1838353 | 3.6 |
+| Jeonbuk | 1792476 | 3.5 |
+| Chungbuk | 1597179 | 3.1 |
+| Gangwon | 1536270 | 3.0 |
+| Daejeon | 1454679 | 2.8 |
+| Gwangju | 1441970 | 2.8 |
+| Ulsan | 1124459 | 2.2 |
+| Jeju | 675883 | 1.3 |
+| Sejong | 365309 | 0.7 |
+
+### Korean COVID-19 new cases by Region
+* Total new cases: 1714
+| Region | New Cases | Ratio (%) | New Cases / 1M |
+| ------ | --------- | --------- |----------------|
+| Seoul | 644 | 0.2 | 67.4 |
+| Gyeongi | 529 | 0.2 | 39.1 |
+| Busan | 38 | 0.2 | 11.3 |
+| Gyeongnam | 29 | 0.2 | 8.7 |
+| Incheon | 148 | 0.2 | 50.4 |
+| Gyeongbuk | 28 | 0.2 | 10.6 |
+| Daegu | 41 | 0.2 | 17.1 |
+| Chungnam | 62 | 0.2 | 29.3 |
+| Jeonnam | 23 | 0.2 | 12.5 |
+| Jeonbuk | 27 | 0.2 | 15.1 |
+| Chungbuk | 27 | 0.2 | 16.9 |
+| Gangwon | 33 | 0.2 | 21.5 |
+| Daejeon | 16 | 0.2 | 11.0 |
+| Gwangju | 40 | 0.2 | 27.7 |
+| Ulsan | 20 | 0.2 | 17.8 |
+| Jeju | 5 | 0.2 | 7.4 |
+| Sejong | 4 | 0.2 | 10.9 |
